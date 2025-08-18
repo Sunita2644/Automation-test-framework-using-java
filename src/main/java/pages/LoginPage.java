58fce1a0cf53d1019b7cd39894715ea11a49914e
Sunita2644 <sunita.tanawade@yahoo.com>
2025-08-18 19:34:42 +0530

    added login test for valid credentials

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,37 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class LoginPage {
+	private WebDriver driver;
+
+    // Locators
+    private By usernameInput = By.id("user-name");
+    private By passwordInput = By.id("password");
+    private By loginButton   = By.xpath("//input[@type=\"submit\"]");
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Actions
+    public void enterUsername(String username) {
+        driver.findElement(usernameInput).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordInput).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,28 @@
 package tests;
 
-public class LoginTest {
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+	@Test
+	public void testValidLogin() {
+        LoginPage loginPage = new LoginPage(driver);
+        try {
+        	loginPage.login("standard_user", "secret_sauce");
+        	String Actual_text = driver.findElement(By.xpath("//span[@class=\"title\"]")).getText();
+        	String expected_text = "Products";
+        	Assert.assertEquals(Actual_text,expected_text, "Login failed");
+        }
+        catch (Exception e){
+        	System.out.println(e);
+        }
+    }
 
 }
